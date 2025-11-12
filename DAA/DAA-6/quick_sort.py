1dac5d014de19f140dc39e4e4895ad21165a6459
Avanish More <avanishmore25@gmail.com>
2025-11-13 00:35:23 +0530

    made easy
    Removed the QuickSort class and its methods, replacing them with standalone deterministic and randomized quick sort functions. Added example usage at the end of the file.

--- DAA/DAA-6/quick_sort.py
+++ DAA/DAA-6/quick_sort.py
@@ -1,62 +1,28 @@
 import random
 
-
-class QuickSort:
-    def __init__(self,array):
-        self.array = array
-    
-
-    # Deterministic method to find pivot
-    def partition(self,low,high):
-        # High element is default pivot
-        pivot = self.array[high]
-
-        # pointer for greater element
-        i = low - 1
+# Deterministic Quick Sort (always picks last element as pivot)
+def deterministic_quick_sort(arr):
+    if len(arr) <= 1:
+        return arr
+    pivot = arr[-1]  # last element as pivot
+    left = [x for x in arr[:-1] if x <= pivot]
+    right = [x for x in arr[:-1] if x > pivot]
+    return deterministic_quick_sort(left) + [pivot] + deterministic_quick_sort(right)
 
 
-        for j in range(low,high):
-            if self.array[j] <= pivot:
-                # if element smaller than pivot is found
-                # swap it with the greater element pointed by i
-                i += 1
-                self.array[i],self.array[j] = self.array[j],self.array[i]
-        # swap the pivot element with the greater element specified by i
-        self.array[i+1],self.array[high] = self.array[high],self.array[i+1]
-
-        return i+1
-    
-    def partition_random(self,low,high):
-        pivot = random.randint(low,high)
-        self.array[pivot],self.array[high] = self.array[high],self.array[pivot]
-        return self.partition(low,high)
-    
-    # deterministic variant of sort
-    def sort_d(self,low,high):
-        if low < high:
-
-            pivot = self.partition(low,high)
-            self.sort_d(low,pivot-1)
-            self.sort_d(pivot+1,high)
-    
-    
-    # random variant of sort
-    def sort_r(self,low,high):
-        if low < high:
-            pivot = self.partition_random(low,high)
-            self.sort_r(low,pivot-1)
-            self.sort_r(pivot+1,high)
+# Randomized Quick Sort (picks random pivot)
+def randomized_quick_sort(arr):
+    if len(arr) <= 1:
+        return arr
+    pivot = random.choice(arr)  # random pivot
+    left = [x for x in arr if x < pivot]
+    mid = [x for x in arr if x == pivot]
+    right = [x for x in arr if x > pivot]
+    return randomized_quick_sort(left) + mid + randomized_quick_sort(right)
 
 
-while True:
-    print("Press Ctrl+C to exit...")
-    array = [int(i) for i in input("Enter array").split(" ")]
-    print("Deterministic variant of sort")
-    sort1 = QuickSort(array)
-    sort1.sort_d(0,len(array)-1)
-    print(sort1.array)
-    print("Radomized variant of sort")
-    sort2 = QuickSort(array)
-    sort2.sort_r(0,len(array)-1)
-    print(sort2.array)
-    
+# Example Input
+arr = [10, 7, 8, 9, 1, 5]
+print("Original Array:", arr)
+print("Deterministic Quick Sort:", deterministic_quick_sort(arr))
+print("Randomized Quick Sort:", randomized_quick_sort(arr))
